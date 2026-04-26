33b19a25d47f64d90a495dd0ff2715a1e2e8960d
Bassim <bassim.matar@gmail.com>
2026-04-26 14:45:10 +0200

    Remove _preview.py as it is no longer needed for the project

--- _preview.py
+++
@@ -1,9 +0,0 @@
-import json
-from download import target_path
-
-with open("out/resources_probed.jsonl") as f:
-    for i, line in enumerate(f):
-        if i >= 5:
-            break
-        r = json.loads(line)
-        print(target_path(r))
